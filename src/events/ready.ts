616bc1a5f9ceb3126afdd30689946bb997b9fee8
RealBlueta <jejojora2@gmail.com>
2022-07-30 18:55:12 -0400

    Run Prettier on ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,7 +3,9 @@ import Client from '../components/client';
 
 export default async function (client: Client) {
 	const GUILD_ID = '1002745278459293706';
-	const GUILD_COMMANDS: SlashCommandBuilder[] = [...client.commands].map((command) => command.data);
+	const GUILD_COMMANDS: SlashCommandBuilder[] = [...client.commands].map(
+		(command) => command.data
+	);
 	client.guilds.cache.get(GUILD_ID)?.commands.set(GUILD_COMMANDS);
 	console.log(`${client.user!.tag} has logged in!`);
 }
