94b6130696ba7ad148500037e0dd735764ac0844
RealBlueta <jejojora2@gmail.com>
2022-07-30 23:09:13 -0400

    Add attatchment requirement for codify command

--- src/commands/codify.ts
+++ src/commands/codify.ts
@@ -3,9 +3,15 @@ import { Command } from '../types';
 import { LCCH } from '../util';
 
 export default new (class implements Command {
-	data: SlashCommandBuilder = new SlashCommandBuilder()
+	data = new SlashCommandBuilder()
 		.setName('codify')
-		.setDescription('Convert PNG image into LCCH code');
+		.setDescription('Convert PNG image into LCCH code')
+		.addAttachmentOption((option) =>
+			option
+				.setName('input')
+				.setDescription('The PNG image of the crosshair (MAX 37x37)')
+				.setRequired(true)
+		);
 
 	async run(interaction: ChatInputCommandInteraction) {
 		// takes image and makes code
