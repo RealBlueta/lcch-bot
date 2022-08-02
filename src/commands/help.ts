583b636c6abc704db7782189a69b6346327c3967
RealBlueta <jejojora2@gmail.com>
2022-08-02 15:45:38 -0400

    put embed in help command, straight into the interaction reply

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -11,19 +11,22 @@ export default new (class implements Command {
 	async run(interaction: ChatInputCommandInteraction) {
 		const client = interaction.client as Client;
 		const commands = [...client.commands].map((command) => '/' + command.data.name);
-		const embed = new EmbedBuilder()
-			.setColor(EMBED_COLOR)
-			.setAuthor({
-				iconURL:
-					'https://images-ext-2.discordapp.net/external/sgPVB2yZDBSt09iaBKvh5L-DXQfYWb4v91Urg7C0450/https/i.imgur.com/bMPc2TT.png', // wrench icon from discord
-				name: 'Commands',
-			})
-			.setDescription(
-				`List of Usable Bot Slash Commands:\n\`\`\`${commands.join('\n')}\`\`\``
-			)
-			.setFooter({
-				text: new Date().toUTCString(),
-			});
-		return interaction.reply({ embeds: [embed] });
+		return interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setColor(EMBED_COLOR)
+					.setAuthor({
+						iconURL:
+							'https://images-ext-2.discordapp.net/external/sgPVB2yZDBSt09iaBKvh5L-DXQfYWb4v91Urg7C0450/https/i.imgur.com/bMPc2TT.png', // wrench icon from discord
+						name: 'Commands',
+					})
+					.setDescription(
+						`List of Usable Bot Slash Commands:\n\`\`\`${commands.join('\n')}\`\`\``
+					)
+					.setFooter({
+						text: new Date().toUTCString(),
+					}),
+			],
+		});
 	}
 })();
