0725ae406aafab131246e876e4e08795cd01c812
RealBlueta <jejojora2@gmail.com>
2022-08-02 15:03:08 -0400

    x

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,10 +2,14 @@ import {} from 'discord.js';
 import Client from '../components/client';
 
 export default async function ready(client: Client) {
+	// TEMPORARY FOR TESTING
 	const GUILD_ID = '1002745278459293706';
 	const GUILD_COMMANDS = [...client.commands]
 		.map((command) => command.data)
 		.map((data) => data.toJSON());
 	client.guilds.cache.get(GUILD_ID)?.commands.set(GUILD_COMMANDS);
 	console.log(`${client.user!.tag} has logged in!`);
+
+	// // x
+	// client.application!.commands.set([]);
 }
