4dbc4420f851a0047af501b7e7bd71432a7a108e
RealBlueta <jejojora2@gmail.com>
2022-08-02 16:15:25 -0400

    Set Bot status!

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,13 @@
+import { ActivityType } from 'discord.js';
 import Client from '../components/client';
 
 export default async function ready(client: Client) {
+	client.user!.setActivity('Lunar Client', {
+		type: ActivityType.Playing,
+	});
+	client.user!.setStatus('idle');
 	console.log(`${client.user!.tag} has logged in!`);
-	
+
 	// (DO ONLY ONCE FOR GLOBAL COMMANDS)
 	// const BOT_COMMANDS = [...client.commands]
 	// 	.map((command) => command.data)
